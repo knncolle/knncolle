1a61d4680cf9d1d24c080632ae13df951d871332
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-08 14:15:56 -0700

    Namespace the typedefs.

--- include/knncolle/utils/utils.hpp
+++ include/knncolle/utils/utils.hpp
@@ -1,6 +1,8 @@
 #ifndef KNNCOLLE_UTILS_H
 #define KNNCOLLE_UTILS_H
 
+namespace knncolle {
+
 // Integer typedefs to avoid grief when comparing types.
 
 typedef size_t VecSize_t;
@@ -10,5 +12,7 @@ typedef int MatDim_t;
 typedef int CellIndex_t;
 
 typedef int NumNeighbors_t;
+
+}
 
 #endif
