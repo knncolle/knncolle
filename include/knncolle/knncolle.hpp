bc3b940cdc8dc7d8c04a5c477305cd11ea8ddf16
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-09-24 23:50:50 -0700

    Add macros to allow skipping of certain headers.

--- include/knncolle/knncolle.hpp
+++ include/knncolle/knncolle.hpp
@@ -2,16 +2,30 @@
 #define KNNCOLLE_HPP
 
 #include "BruteForce/BruteForce.hpp"
-#include "Kmknn/Kmknn.hpp"
 #include "VpTree/VpTree.hpp"
+
+#ifndef KNNCOLLE_NO_KMKNN
+#include "Kmknn/Kmknn.hpp"
+#endif
+
+#ifndef KNNCOLLE_NO_ANNOY
 #include "Annoy/Annoy.hpp"
+#endif
+
+#ifndef KNNCOLLE_NO_HNSW
 #include "Hnsw/Hnsw.hpp"
-#include <memory>
+#endif
 
 /**
  * @file knncolle.hpp
  *
  * @brief Umbrella header to include all algorithms.
+ *
+ * Developers can avoid the inclusion of unnecessary dependencies by setting:
+ *
+ * - `KNNCOLLE_NO_KMKNN`, to avoid including the `Kmknn.hpp` header (which requires the **kmeans** library).
+ * - `KNNCOLLE_NO_ANNOY`, to avoid including the `Annoy.hpp` header (which requires the **Annoy** library).
+ * - `KNNCOLLE_NO_HNSW`, to avoid including the `Hnsw.hpp` header (which requires the **Hnsw** library).
  */
 
 #endif
