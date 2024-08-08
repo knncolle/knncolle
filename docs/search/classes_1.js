bd59b834d4db5f243251b8722c1d40ad34890350
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-08 22:27:44 +0000

    Deploying to gh-pages from @ knncolle/knncolle@d8a82ae74a5295f22d3ce32289d57d3f8d532de0 🚀

--- docs/search/classes_1.js
+++ docs/search/classes_1.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['consecutiveaccessworkspace_0',['ConsecutiveAccessWorkspace',['https://ltla.github.io/CppKmeans/structkmeans_1_1MockMatrix_1_1ConsecutiveAccessWorkspace.html',1,'kmeans::MockMatrix']]],
-  ['cosinedistance_1',['CosineDistance',['../structknncolle_1_1CosineDistance.html',1,'knncolle']]]
+  ['consecutiveaccessworkspace_0',['ConsecutiveAccessWorkspace',['https://ltla.github.io/CppKmeans/structkmeans_1_1MockMatrix_1_1ConsecutiveAccessWorkspace.html',1,'kmeans::MockMatrix']]]
 ];
